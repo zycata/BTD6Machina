1db81b14bb5446da5b7c05498209654b16be7545
zycata <hyperconger@gmail.com>
2025-05-17 19:56:31 -0700

    linked list for fun

--- test2/test.cpp
+++ test2/test.cpp
@@ -1,7 +1,33 @@
 
 // my_lib.cpp
-extern "C" {
-    __declspec(dllexport) int add(int a, int b) {
-        return a + b;
+#include <iostream>
+
+using namespace std;
+
+struct Head {
+    int a;
+    Head *next;
+};
+
+
+
+int main() {
+    Head *head = new Head;
+    head->a = 1;
+    head->next = nullptr;
+    Head *current = head;
+    for (int i = 0; i < 5; ++i) {
+        Head *newNode = new Head;
+        newNode->a = i + 2;
+        newNode->next = nullptr;
+        current->next = newNode;
+        current = newNode;
     }
+    // PRINT out the linked list vals
+    current = head;
+    while (current != nullptr) {
+        cout << current->a << " ";
+        current = current->next;
+    }
+    return 0;
 }
