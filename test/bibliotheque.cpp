5fb27b78579b8f0f2348c1e2bcd5f233bb7563d4
zycata <hyperconger@gmail.com>
2025-05-18 20:56:41 -0700

    test stuff

--- test/bibliotheque.cpp
+++ test/bibliotheque.cpp
@@ -5,6 +5,7 @@
 #include <ctime>
 #include <vector>
 #include <map>
+#include <random>
 namespace bibliotheque {
     int yap() {
         std::cout << "Hello from bibliotheque" << std::endl;
@@ -33,14 +34,21 @@ int roundToNearest5(int num, float multiplier) {
 
 
 
+
+
+int getRandomInt(int n1, int n2) {
+    // srand(time(nullptr));  // seed (only once in main ideally)
+    return n1 + rand() % (n2 - n1 + 1);
+}
+
 int main() {
     
     srand(time(0));
     vector<int> randomints;
     map<int, int> randintmap;
 // Generate a random number between 0 and 9
-    for (int i = 0; i < 1000; ++i) {
-        int randomNum = rand() % 2;
+    for (int i = 0; i < 100; ++i) {
+        int randomNum = getRandomInt(5, 9);
         randomints.push_back(randomNum);
     }
     
