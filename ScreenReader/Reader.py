728595387c00d449831d0ab1ab0292a9ec1fab21
zycata <hyperconger@gmail.com>
2025-07-05 22:21:34 -0700

    changes to reading data n stuff and other fun knee

---
+++ ScreenReader/Reader.py
@@ -0,0 +1,10 @@
+import pyautogui
+import pygetwindow as gw
+import time
+import cv2
+from PIL import Image # Make sure you import Image if you're loading from a file
+
+
+
+isContinue = pyautogui.locateOnScreen('ScreenReader/Images/NextRound.png')
+
