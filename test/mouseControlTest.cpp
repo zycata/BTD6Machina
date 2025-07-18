ca36bc40594ff3eaeee76660519e870503ebb157
zycata <hyperconger@gmail.com>
2025-07-17 23:45:57 -0700

    SOY admen

---
+++ test/mouseControlTest.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "../src/mouseControl/mouseControl.h"
+#include <ctime>
+#include <windows.h>
+//impoppable mode on monkey meadows btw...
+// g++ test/mouseControlTest.cpp src/mouseControl/mouseControl.cpp -o mouseControlTest.exe
+using namespace mouseControl;
+
+void testPlaceAndUpgrade() {
+    int x = 690;
+    int y = 60;
+    int topPath = 0;
+    int botPath = 2;
+    placeTower(1, x, y);
+    upgradeTower(x, y, topPath);
+    upgradeTower(x, y, botPath);
+
+}
+
+void testStartRound() {
+    ClickStartNextRound();
+    ClickStartNextRound();
+}
+
+void restartGame() {
+    restartGameWhenOver(false);
+}
+int main() {
+    initializeMouseControls();
+    testPlaceAndUpgrade();
+    testStartRound();
+    Sleep(7000);
+    restartGame();
+    return 0;
+}
