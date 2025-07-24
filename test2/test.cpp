2b5570b59f9c5050161f3fd801ab4c85dd9ddfde
zycata <hyperconger@gmail.com>
2025-07-23 21:33:58 -0700

    hello giuys welcome to my miencraft letes play today we will be mining for diamonds

--- test2/test.cpp
+++ test2/test.cpp
@@ -1,33 +1,12 @@
 
 // my_lib.cpp
 #include <iostream>
+#include<vector>
 
 using namespace std;
 
-struct Head {
-    int a;
-    Head *next;
-};
-
-
 
 int main() {
-    Head *head = new Head;
-    head->a = 1;
-    head->next = nullptr;
-    Head *current = head;
-    for (int i = 0; i < 5; ++i) {
-        Head *newNode = new Head;
-        newNode->a = i + 2;
-        newNode->next = nullptr;
-        current->next = newNode;
-        current = newNode;
-    }
-    // PRINT out the linked list vals
-    current = head;
-    while (current != nullptr) {
-        cout << current->a << " ";
-        current = current->next;
-    }
-    return 0;
+    vector<int> heya = {0 , 1, 2};
+    cout << heya[1] << endl;
 }
